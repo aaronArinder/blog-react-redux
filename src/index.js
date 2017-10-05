2eb1adbb7ea88b4778421708dba8f5d9d06ee798
Aaron Arinder <aaronarinder@gmail.com>
2017-10-05 13:21:44 -0400

    added <Switch> to handle routes appropriately

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PostsIndex from './components/posts-index';
 import PostsNew from './components/posts-new';
@@ -13,13 +13,17 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 //path and component are required props for every Route component; i.e., if this path, show this component.
+// //react-router fuzzily matches routes, and won't stop at the first thing it matches; we need to include
+// Switch, which will match one and only one route. So, put most speficic routes up top.
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
