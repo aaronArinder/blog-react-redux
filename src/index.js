cd21344b397a6e924e8529ee64f1e102c10cf563
Aaron Arinder <aaronarinder@gmail.com>
2017-10-05 13:13:59 -0400

    added PostsNew and its routing

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import PostsIndex from './components/posts-index';
-import promise from 'redux-promise';
 
+import PostsIndex from './components/posts-index';
+import PostsNew from './components/posts-new';
+
+import promise from 'redux-promise';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -17,6 +19,7 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
     </BrowserRouter>
   </Provider>,
